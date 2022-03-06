d3f14cac3b6baa3fb92e6a969f28d8937c8f8645
KingYoSun <nocking257838@gmail.com>
2022-03-07 08:23:29 +0900

    contextBridge化

--- main/preload.ts
+++ main/preload.ts
@@ -2,6 +2,6 @@ import { ipcRenderer, IpcRenderer, contextBridge } from "electron";
 
 contextBridge.exposeInMainWorld("electron", {
   sayMsg: async (message: string) => {
-    return await ipcRenderer.invoke("sayMsg", "test").then((result) => result);
+    return await ipcRenderer.invoke("sayMsg", message).then((result) => result);
   },
 });
